46275e740f39d8bd7ef9c212fdff16b4208e75c3
e-pas <epasekunov@gmail.com>
2023-03-29 20:10:09 +0300

    Iter8 first commit
    Added gzip support

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,8 +1,11 @@
 package app
 
 import (
+	"compress/gzip"
+	"io"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/e-pas/yandex-praktikum-shortener/internal/app/config"
 	"github.com/e-pas/yandex-praktikum-shortener/internal/app/endpoint"
@@ -31,6 +34,7 @@ func New() (*App, error) {
 	a.r.Use(middleware.RealIP)
 	a.r.Use(middleware.Logger)
 	a.r.Use(middleware.Recoverer)
+	a.r.Use(gzipHandle)
 
 	a.r.Get("/{id}", a.e.Get)
 	a.r.Get("/info", a.e.Info)
@@ -44,4 +48,31 @@ func (a *App) Run() error {
 	log.Println("service running")
 	err := http.ListenAndServe(a.c.Listen, a.r)
 	return err
+}
+
+type gzipWrite struct {
+	http.ResponseWriter
+	gzWriter io.Writer
+}
+
+func (gz gzipWrite) Write(buf []byte) (int, error) {
+	return gz.gzWriter.Write(buf)
+}
+
+func gzipHandle(next http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
+			next.ServeHTTP(w, r)
+			return
+		}
+		gzw := gzip.NewWriter(w)
+		defer gzw.Close()
+		gz := gzipWrite{
+			ResponseWriter: w,
+			gzWriter:       gzw,
+		}
+		w.Header().Set("Content-Encoding", "gzip")
+		next.ServeHTTP(gz, r)
+	}
+	return http.HandlerFunc(fn)
 }
